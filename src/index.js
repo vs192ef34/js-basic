2f37823ab3906cf30e98627cc852a493256c3c57
Vlad <vskrashchuk@gmail.com>
2020-06-26 21:28:08 +0300

    Show variables and constans declaration

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const greeting = "Hello";
+const daysInWeek = 7;
+const pi = 3.1415926;
+
+let daysInJune = 29;
+
+console.log(`Greeting = ${greeting}`);
+console.log(`Days in Week: ${daysInWeek}`);
+console.log(`PI value is ${pi}`);
+
+daysInJune = 30;
+
+console.log(`Days in June: ${daysInJune}`);
